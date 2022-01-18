9ee227aa6e8f65da8b2de4c17b42ba0469f49c26
codekyz <codekyz0111@gmail.com>
2022-01-18 21:07:13 +0900

    :tada: Begin a project
    Todolist 만들기 시작

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const deleteButton = document.querySelector('.del_btn');
+const addButton = document.querySelector('.add_btn');
+
+const itemValue = document.querySelector('#item');
+const memoValue = document.querySelector('#memo');
+
+const checkBox = document.querySelector('.checkbox');
+const todoValue = document.querySelector('.todo_value');
+
+
+function addItem() {
+    
+}
+
+function delItem() {
+    
+}
+
+addButton.addEventListener('click', addItem);
+deleteButton.addEventListener('click', delItem);
